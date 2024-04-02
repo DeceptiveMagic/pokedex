a521b20c271984c350bfd8110178c218e73a9075
Kyle <kylefink3@gmail.com>
2024-04-02 12:39:57 -0400

    Started writing basic functions and basic layout

--- pokedex.js
+++ pokedex.js
@@ -1,17 +1,37 @@
-function showMenu(){
 
+const number;
+
+function showMenu(){
+	console.log("***************************************");
+	console.log("1: Search Pokemon");
+	console.log("2: Search Move");
+	console.log("3: Search Item");
+	console.log("***************************************");
 }
 
 function prompt(cb){
-
+	rl.question("Please enter command number: ", (number))
+	switch (number){
+		case 1:	
+			rl.question("Please enter Pokemon Name: ", (response))
+		break;
+		case 2:	
+			rl.question("Please enter Pokemon Move: ", (response))
+		break;
+		case 3:	
+			rl.question("Please enter Item Name: ", (response))
+		break;
+	}
 }
 
 function searchPoke(term){
-
+	
 }
 
 function printPoke(json){
-
+	(json) => {
+		console.log(json);
+	}	
 }
 
 function searchItem(term){
@@ -19,7 +39,9 @@ function searchItem(term){
 }
 
 function printItem(json){
-
+	(json) => {
+		console.log(json);
+	}	
 }
 
 function searchMove(term){
@@ -27,9 +49,13 @@ function searchMove(term){
 }
 
 function printMove(json){
-
+	(json) => {
+		console.log(json);
+	}	
 }
 
 function run(){
-
+	showMenu() => {
+		prompt();
+		}
 }

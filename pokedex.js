2213d68e657e562d9ea7fb69886d2ff4efbe4dc4
DeceptiveMagic <deceptivemagic9@gmail.com>
2024-04-03 09:57:55 -0400

    imports and pokedex prefix at top

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,16 @@
+// Load the readline library
+const readline = require("readline");
+// Setup readline to listen on the stdin stream
+const rl = readline.createInterface(process.stdin, process.stdout);
+//setup the response
+const response;
+
+
+//The start to all API fetch requests:
+//https://pokeapi.co/api/v2/
+const apiSetup = "https://pokeapi.co/api/v2/";
+
+
 function showMenu(){
 
 }
